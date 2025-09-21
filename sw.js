b8e5cfae99504fcce3d461da37cdc2ee093e40af
younesdjabali <younesdjabali01@gmail.com>
2025-09-21 18:32:54 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+
+const CACHE_NAME = 'pyramide-v1';
+const ASSETS = [
+  './',
+  'index.html',
+  'manifest.webmanifest',
+  'icon-192.png',
+  'icon-512.png'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.map(k => k !== CACHE_NAME && caches.delete(k))))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((r) => r || fetch(e.request))
+  );
+});
